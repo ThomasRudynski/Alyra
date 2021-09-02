ba4031fdafc6660ccebcc44035cb5a4e55456fa5
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-02 15:24:37 +0200

    Create WhitelistFinal.sol

---
+++ solidityExercices/WhitelistFinal.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+contract Whitelist {
+    
+    mapping(address => bool) whitelist;
+    
+    event Authorized(address _address);
+    
+    function authorize(address _address) public{
+        whitelist[_address] = true;
+        emit Authorized(_address);
+    }
+    
+    
+}
