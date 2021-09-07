dcbc3eaf3f9c1a3381a837e64374357b79f8f85f
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-07 09:53:39 +0200

    Update Voting.sol

--- solidityExercices/solidityChallenge/Voting.sol
+++ solidityExercices/solidityChallenge/Voting.sol
@@ -8,6 +8,9 @@ contract Voting is Ownable{
 
     //uints
     uint winningProposalId;
+    uint proposalId = 1;
+    uint voteNumber;
+
 
     //Structs
     struct Voter {
@@ -44,8 +47,25 @@ contract Voting is Ownable{
 
     //Mappings
     mapping(address => bool) whitelist;
+    mapping(uint => Proposal) proposals;
+    mapping(address => uint) votes;
 
     //Modifiers
+     modifier ownerWhitelist(){
+        require(msg.sender == owner(), "Only contract owner can whitelist");   
+        _;
+    }
+
+    modifier ownerStartWhitelist(){
+        require(msg.sender == owner(), "Only contract owner can start whitelist registration");
+        _;
+    }
+
+    modifier ownerEndWhitelist(){
+        require(msg.sender == owner(), "Only contract owner can end whitelist registration");
+        _;
+    }
+
     modifier ownerStartProposalRegistration(){
         require(msg.sender == owner(), "Only contract owner can start proposal registration");
         _;
@@ -56,18 +76,80 @@ contract Voting is Ownable{
         _;
     }
 
-    //Functions
-    function startProposalRegistration() public ownerStartProposalRegistration {
-        emit ProposalsRegistrationStarted();
+    modifier ownerStartVotingSession(){
+        require(msg.sender == owner(), "Only contract owner can start voting session");
+        _;
     }
 
-    function endProposalRegistration() public ownerEndProposalRegistration {
-        emit ProposalsRegistrationEnded();
+    modifier ownerEndVotingSession(){
+        require(msg.sender == owner(), "Only contract owner can end voting session");
+        _;
+    }
+
+    modifier ownerCalculateVotes(){
+        require(msg.sender == owner(), "Only contract owner can calculate votes");
+        _;
+    }
+
+    //Functions
+
+    function startWhitelistRegistration() public ownerStartWhitelist {
+        emit WorkflowStatusChange(0, RegisteringVoters);
+    }
+
+    function whitelist(address _address) public ownerWhitelist {
+        require(whitelist[_address], "Address already whitelisted");
+        whitelist[_address] = true;
+    }
+
+
+    function startProposalRegistration() public ownerStartProposalRegistration {
+        emit ProposalsRegistrationStarted();
+        emit WorkflowStatusChange(RegisteringVoters, ProposalsRegistrationStarted);
     }
 
     function addProposal(string description) external {
         require(true == whitelist(msg.sender), "You're not whitelisted");
         Proposal memory proposal = Proposal(description,0);
+        proposals[proposalId] = proposal;
         emit ProposalRegistered(proposalId);
+        proposalId++;
     }
+
+    function endProposalRegistration() public ownerEndProposalRegistration {
+        emit ProposalsRegistrationEnded();
+        emit WorkflowStatusChange(ProposalsRegistrationStarted, ProposalsRegistrationEnded);
+    }
+
+
+
+    function startVotingSession() public ownerStartVotingSession {
+        emit VotingSessionStarted();
+        emit WorkflowStatusChange(ProposalsRegistrationEnded, VotingSessionStarted);
+    }
+
+    function vote(uint proposalIndex) external{
+        require(true == whitelist(msg.sender),"You're not whitelisted");
+        require(votes[msg.sender != 0],"You have already voted");
+        require(proposals[uint] != 0,"This proposal does not exist");
+        votes[msg.sender] = proposalIndex;
+        emit Voted(msg.sender, proposalIndex);
+        voteNumber++;
+    }
+
+    function endVotingSession() public ownerEndVotingSession {
+        emit VotingSessionEnded();
+        emit WorkflowStatusChange(VotingSessionStarted, VotingSessionEnded);
+    }
+
+
+
+    function votesCalculation() public ownerCalculateVotes {
+        for (uint i=0; i < voteNumber; i++){
+            proposals[votes[i]].voteCount++;
+        }
+        emit VotesTallied();
+        emit WorkflowStatusChange(VotingSessionEnded, VotesTallied);
+    }
+
 }
