ee2cf35849a56d50e77e4383844e45ce3572db34
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-16 12:29:06 +0200

    Change Test Infura

--- web3Exercices/testInfura.js
+++ web3Exercices/testInfura.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
 const rpcURL = "https://ropsten.infura.io/v3/334fe0b193374db486f2de7564dc4343"
 const web3 = new Web3(rpcURL)
 
-web3.eth.getBalance("0xb8c74A1d2289ec8B13ae421a0660Fd96915022b1", (err, wei) => { 
+web3.eth.getBalance("0x06688FEdB6188Ca3E84765b8984c1dF7236675b1", (err, wei) => { 
    balance = web3.utils.fromWei(wei, 'ether'); // convertir la valeur en ether
    console.log(balance);
 });
