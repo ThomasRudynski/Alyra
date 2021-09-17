a408da9b8f20380f125c1527a9042eb5a1c679b7
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-17 12:09:06 +0200

    Web3 Exemples

---
+++ web3Exemples/getSmartContractInfo.js
@@ -0,0 +1,12 @@
+var  Web3  =  require('web3');  
+web3  =  new  Web3(new  Web3.providers.HttpProvider('https://mainnet.infura.io/v3/VOTRE_ID_PROJET'));
+
+console.log('Calling Contract.....');
+
+var  abi  =  ABI-JSON-INTERFACE;
+var  addr  =  "CONTRACT-ADDRESS";
+
+var  Contract  =  new  web3.eth.Contract(abi, addr);
+
+// FUNCTION must the name of the function you want to call. 
+Contract.methods.FUNCTION().call().then(console.log);
