b4ed6bcf355ffa270eb49d1eb6e8192283382d80
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-17 12:19:39 +0200

    Update getAddressBTU.js

--- web3Exemples/getAddressBTU.js
+++ web3Exemples/getAddressBTU.js
@@ -1,18 +1,18 @@
-var  Web3 = require('web3');
+var Web3 = require('web3');
 web3 = new  Web3(new  Web3.providers.HttpProvider('https://mainnet.infura.io/v3/334fe0b193374db486f2de7564dc4343'));
     
 console.log('Contract tokens balance.....');
-var  addr = ('0xd804ab1667e940052614a5acd103dde4d298ce36');
-console.log("Address: "+addr);
+var address = ('0xd804ab1667e940052614a5acd103dde4d298ce36');
+console.log("Address: "+address);
     
-var  contractAddr = ('0xb683d83a532e2cb7dfa5275eed3698436371cc9f');
-var  tokenAddress = (addr).substring(2); //Remove "0x"
+var contractAddr = ('0xb683d83a532e2cb7dfa5275eed3698436371cc9f');
+var tokenAddress = (address).substring(2); //Remove "0x"
     
 //Balance of function call
-var  contractData = ('0x70a08231000000000000000000000000'+tokenAddress);
+var contractData = ('0x70a08231000000000000000000000000'+tokenAddress);
     
 web3.eth.call({
-	to: contractAddr,
+	to: contractAddress,
 	data: contractData 
 }, function(err, result) {
 	if (result) {
