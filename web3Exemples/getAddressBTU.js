5ab33f331323507f58dbea483cad6bfbdd2f8d0c
ThomasRudynski <thomas.rudynski@gmail.com>
2021-09-17 12:15:58 +0200

    Update getAddressBTU.js

--- web3Exemples/getAddressBTU.js
+++ web3Exemples/getAddressBTU.js
@@ -1,23 +1,23 @@
-var  Web3  =  require('web3');
-web3  =  new  Web3(new  Web3.providers.HttpProvider('https://mainnet.infura.io/v3/334fe0b193374db486f2de7564dc4343'));
+var  Web3 = require('web3');
+web3 = new  Web3(new  Web3.providers.HttpProvider('https://mainnet.infura.io/v3/334fe0b193374db486f2de7564dc4343'));
     
 console.log('Contract tokens balance.....');
-var  addr  = ('0xd804ab1667e940052614a5acd103dde4d298ce36');
-console.log("Address: "  +  addr);
+var  addr = ('0xd804ab1667e940052614a5acd103dde4d298ce36');
+console.log("Address: "+addr);
     
-var  contractAddr  = ('0xb683d83a532e2cb7dfa5275eed3698436371cc9f');
-var  tknAddress  = (addr).substring(2); //Remove "0x"
+var  contractAddr = ('0xb683d83a532e2cb7dfa5275eed3698436371cc9f');
+var  tokenAddress = (addr).substring(2); //Remove "0x"
     
 //Balance of function call
-var  contractData  = ('0x70a08231000000000000000000000000'  +  tknAddress);
+var  contractData = ('0x70a08231000000000000000000000000'+tokenAddress);
     
 web3.eth.call({
 	to: contractAddr,
 	data: contractData 
 }, function(err, result) {
 	if (result) {
-		var  tokens  =  web3.utils.toBN(result).toString(); 
-		    console.log('Tokens : '  +  web3.utils.fromWei(tokens, 'ether')); //Balance
+		var  tokens = web3.utils.toBN(result).toString(); 
+		    console.log('Tokens : '+web3.utils.fromWei(tokens, 'ether')); //Balance
 	}
 	else {
 		console.log(err); // Errors
